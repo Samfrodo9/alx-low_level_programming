1464df3185b54e5621f7137fce8775e846a7f9cb
Samfrodo9 <adeyemifrodo@gmail.com>
2022-07-05 22:44:57 +0100

    Write a function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-#include <ctype.h>
 
 /**
- * print_sign - check if character is lowercase
- * @n: is an integer value
- * Return: 0 always
+ * print_sign -> prints sign based on the condition
+ * @n: argument passed
+ * Return: 1, 0, -1
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -17,6 +16,7 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
