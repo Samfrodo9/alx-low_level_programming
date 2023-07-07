a03af4278a5e302cae548a93a3450f6da81cf074
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-07-07 15:56:26 +0000

    files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 		if ((b[i] < '0') || (b[i] > '1'))
 			return (0);
 
-		result += 2 * result + (b[i] - '0');
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
