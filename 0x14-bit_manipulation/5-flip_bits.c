633f77c04f9fe147196cc362ae2c3969b77e9c55
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-07-07 22:45:43 +0000

    function that returns the number of buts

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+"include "main.h"
+
+/**
+* flip_bits - function that returns the number of buts
+* you will neeed to flip to get another
+* @n: number one
+* @m: number two
+*
+* Return: the number of bits
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bits_num = 0;
+
+	while (n != 0 || m != 0)
+	{
+		if ((n & 1) != (m & 1))
+			bits_num++;
+		n >>= 1;
+		m >>= 1;
+	}
+
+	return bits_num;
+}
