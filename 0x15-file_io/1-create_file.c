03010563860bb7cf83a8062fe25aef081e183c24
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-07-10 22:21:42 +0000

    A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+"include "main.h"
+
+/**
+*create_file - A function that creates a file
+*@filename: the name of file to create
+*@text_content: string to write to file
+*Return: 1 on success, -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if(!text_content)
+		text_content = "";
+
+	for (i = 0; *(text_content + i) != '\0'; i++)
+
+
+	write(fd, filename, i);
+	close(filename);
+
+	return (1);
+}
