48a7ac8ed19ec7483e46d05c246efd9660aaf9bf
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-07-10 22:27:45 +0000

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-"include "main.h"
+#include "main.h"
 
 /**
 *create_file - A function that creates a file
@@ -10,6 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int i;
 
 	if (filename == NULL)
 		return (-1);
@@ -25,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 
 	write(fd, filename, i);
-	close(filename);
+	close(fd);
 
 	return (1);
 }
