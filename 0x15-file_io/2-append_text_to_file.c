1f5eff3f95e6adcca03bb81e397925b2bb64e95d
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-07-10 22:55:21 +0000

    A func that appends to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - a funct that appends to a file
+ * @filename: name of the file
+ * @text_content: text to append
+ * Return: -1 if null and 1 if exixt
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = 0;
+
+	if (text_content == NULL)
+	{
+		fd = open(file_name, O_WRONLY);
+		if (fd == -1)
+			return (-1);
+		else
+			return (1);
+	}
+
+	fd = open(file_name, O_WRONLY || O_APPEND);
+	i = 0;
+
+	while (!text_content[0])
+		i++;
+
+	write(fd, text_content, i);
+
+	close(fd);
+
+	return (1);
+}
