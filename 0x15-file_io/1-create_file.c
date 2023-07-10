edfea02f11e35ca25e0bf3891dd128e5c2209f37
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-07-10 23:04:09 +0000

    Append to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if(!text_content)
+	if (!text_content)
 		text_content = "";
 
 	for (i = 0; *(text_content + i) != '\0'; i++)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,14 +18,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(file_name, O_WRONLY);
+		fd = open(filename, O_WRONLY);
 		if (fd == -1)
 			return (-1);
 		else
 			return (1);
 	}
 
-	fd = open(file_name, O_WRONLY || O_APPEND);
+	fd = open(filename, O_APPEND);
 	i = 0;
 
 	while (!text_content[0])
