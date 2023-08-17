818496ffaa419b49117fbbb9ff5523775cae2f57
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-08-17 22:33:30 +0000

    Write a function that returns length of a dlinked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints a dlinked list
+ * @h: pointer to head of list
+ *
+ * Return: Length of list
+ */
+
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
