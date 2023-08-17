a334600cb87d319f7151bf9152584da34ffe39c6
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-08-17 22:13:08 +0000

    Printing doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	while(h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	printf("%d\n", h->n);
+
+
+	return (i + 1);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# Learning doubly linked list
